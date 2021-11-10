38a0550cd8081892a0db6d8f0fa4c632c9007692
Matej Beno <matobeno1@gmail.com>
2021-11-11 00:11:32 +0100

    Updated eslint rules, installed packages.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,32 @@
 module.exports = {
-  extends: [
-      "next/core-web-vitals",
-      "plugin:react-hooks/recommended",
-      "eslint:recommended",
-      "plugin:react/recommended"
-  ]
-}
+    env: {
+        "browser": true,
+        "es2021": true
+    },
+    extends: [
+        "eslint:recommended",
+        "next/core-web-vitals",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true
+        },
+        ecmaVersion: 13,
+        sourceType: "module"
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
+    plugins: [
+        "react",
+        "@typescript-eslint"
+    ],
+    rules: {
+    }
+};
