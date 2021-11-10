27854df93b3b04e62db68f60ba8b34024352f9d6
Matej Beno <matobeno1@gmail.com>
2021-11-11 00:55:04 +0100

    Configured getStaticPaths for name dyn. page.

--- pages/[name].tsx
+++ pages/[name].tsx
@@ -1,5 +1,6 @@
-import type { NextPage } from "next";
+import type { GetStaticPaths, NextPage } from "next";
 import { useRouter } from "next/router";
+import { fetchPokemonsRequest } from "../src/requests";
 
 const PokemonDetailPage: NextPage = () => {
     const router = useRouter();
@@ -7,6 +8,18 @@ const PokemonDetailPage: NextPage = () => {
     return (
         <h1>{name}</h1>
     );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const data = await fetchPokemonsRequest(9);
+    return {
+        paths: data.results.map(({ name }) => ({
+            params: {
+                name
+            }
+        })),
+        fallback: true,
+    };
 };
 
 export default PokemonDetailPage;
