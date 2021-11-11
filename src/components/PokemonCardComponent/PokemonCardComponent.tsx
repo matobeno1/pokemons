c66432655d32b1d0889eeb6c32e87282a0524db8
Matej Beno <matobeno1@gmail.com>
2021-11-11 14:24:33 +0100

    Creating PokemonCardComponent

---
+++ src/components/PokemonCardComponent/PokemonCardComponent.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from "react";
+import { Pokemon } from "../../types";
+
+type PokemonCardComponentProps = {
+    pokemon: Pokemon
+};
+
+export const PokemonCardComponent: FunctionComponent<PokemonCardComponentProps> = ({
+    pokemon
+}) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
