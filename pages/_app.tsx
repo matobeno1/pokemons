57070c6582b52c222e2c1b749619419028470097
Matej Beno <matobeno1@gmail.com>
2021-11-11 15:46:49 +0100

    Organising stylesheets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import "../styles/globals.scss";
-import "../styles/index.scss";
 import type { AppProps } from "next/app";
+import "../styles/globals.scss";
+import "../styles/pages/index.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
