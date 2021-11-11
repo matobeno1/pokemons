efa90c2a3bfcca8da277dd91ded3c5f3bb0d0b05
Matej Beno <matobeno1@gmail.com>
2021-11-11 13:03:37 +0100

    404 page updated

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
-export default function NotFoundPage() {
-    return (
-        <>
-            <h1>404 - Page Not Found</h1>
-            <Link href="/">
-                <a>
-                    Go back home
-                </a>
-            </Link>
-        </>
-    );
-}
+const NotFoundPage = () => (
+    <>
+        <h1>404 - Page Not Found</h1>
+        <Link href="/">
+            <a>
+                Go back home
+            </a>
+        </Link>
+    </>
+);
+
+export default NotFoundPage;
