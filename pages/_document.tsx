019b50cf432ca2105d8b3b13e746d5a56b2f9814
Matej Beno <matobeno1@gmail.com>
2021-11-16 19:04:14 +0100

    Background color transition

---
+++ pages/_document.tsx
@@ -0,0 +1,24 @@
+import Document, { DocumentContext, Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        return await Document.getInitialProps(ctx);
+    }
+
+    render(): JSX.Element {
+        return (
+            <Html>
+                <Head />
+                <body>
+                <div className="backdrop"/>
+                <div className="content">
+                    <Main />
+                </div>
+                <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
