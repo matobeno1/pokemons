31422988a2db327db2738b48fcd66a37aed2158c
Matej Beno <matobeno1@gmail.com>
2021-11-17 01:34:28 +0100

    Navigation done.

--- src/components/SiteNavigationComponent/SiteNavigationComponent.tsx
+++ src/components/SiteNavigationComponent/SiteNavigationComponent.tsx
@@ -1,11 +1,21 @@
 import { FunctionComponent } from "react";
 import Link from "next/link";
+import Image from "next/image";
 import { createClassNames } from "../../bem";
 
 const classNames = createClassNames("site-navigation-component");
 
 export const SiteNavigationComponent: FunctionComponent = () => (
     <nav className={classNames()}>
-        <Link href="/">Home</Link>
+        <Link href="/" passHref>
+            <a className={classNames("back")}>
+                <span className={classNames("image")}>
+                    <Image src="/pokeball.svg" layout="fill" alt="logo" />
+                </span>
+                <span className={classNames("back-text")}>
+                    Pokedex
+                </span>
+            </a>
+        </Link>
     </nav>
 );
