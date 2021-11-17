e032f427da5a1f79a735792c6ef192d7ef1f255f
Matej Beno <matobeno1@gmail.com>
2021-11-17 17:03:00 +0100

    Updating snapshot test

--- src/components/LoaderComponent/LoaderComponent.test.tsx
+++ src/components/LoaderComponent/LoaderComponent.test.tsx
@@ -1,12 +1,13 @@
 import React from "react";
-import renderer from "react-test-renderer";
+import { act, create, ReactTestRenderer } from "react-test-renderer";
 import { LoaderComponent } from "./LoaderComponent";
 
 describe(LoaderComponent, () => {
-    it("snapshots", () => {
-        const tree = renderer
-            .create(<LoaderComponent />)
-            .toJSON();
-        expect(tree).toMatchSnapshot();
+    it("snapshots", async () => {
+        let root: ReturnType<typeof create> | undefined;
+        act(() => {
+            root = create(<LoaderComponent />);
+        });
+        expect(root?.toJSON()).toMatchSnapshot();
     });
 });
