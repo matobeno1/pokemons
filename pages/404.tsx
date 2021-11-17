9db0523d7f2c0c83ab64942b3558c776b9f8d955
Matej Beno <matobeno1@gmail.com>
2021-11-17 21:19:16 +0100

    404 page update

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,34 @@
 import Link from "next/link";
+import Head from "next/head";
+import Image from "next/image";
+
+import { APP_TITLE } from "@src/constants";
+import { createClassNames } from "@src/bem";
+
+const classNames = createClassNames("page-404");
 
 const NotFoundPage = () => (
     <>
-        <h1>404 - Page Not Found</h1>
-        <Link href="/">
-            <a>
-                Go back home
-            </a>
-        </Link>
+        <Head>
+            <title>{APP_TITLE} - Page not found</title>
+        </Head>
+        <div className={classNames()}>
+            <div className={classNames("content")}>
+                <div className={classNames("title-404")} aria-hidden="true">
+                    <span className={classNames("letter")}>4</span>
+                    <div className={classNames("image")}>
+                        <Image src="/pokeball.svg" layout="fill" />
+                    </div>
+                    <span className={classNames("letter")}>4</span>
+                </div>
+                <h1 className={classNames("title-verbose")}>Page Not Found</h1>
+                <Link href="/">
+                    <a className={classNames("link")}>
+                        Go back home
+                    </a>
+                </Link>
+            </div>
+        </div>
     </>
 );
 
