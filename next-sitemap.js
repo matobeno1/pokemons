b329e0f6b0efb3e2090a7a03bacd50aabbde0bee
Matej Beno <matobeno1@gmail.com>
2022-03-29 09:43:50 +0200

    Adding sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: process.env.SITE_URL || "https://silly-lichterman-cbcdf8.netlify.app",
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+};
